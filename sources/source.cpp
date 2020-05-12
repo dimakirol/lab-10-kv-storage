864d9c80275f304b0b0ddc8bdfd8e149829b32a8
mazi-k <55616346+mazi-k@users.noreply.github.com>
2020-05-13 00:40:04 +0300

    add Travic

--- sources/source.cpp
+++ sources/source.cpp
@@ -5,7 +5,7 @@
 
 class RandomString {
 public:
-    
+//
     RandomString() {
         alpha =
         "abcdefghijklmnopqrstuvwxyz"
@@ -15,15 +15,15 @@ public:
         randomizer = static_cast<uint64_t>(rand_r(&now) % alpha.length());
         random_string.assign(alpha, 0, value_size);
     }
-    
-    std::string SetRandomValue (int ThreadID) {
+
+    std::string SetRandomValue(int ThreadID) {
         randomizer += ThreadID;
         int64_t tmp_randomizer = randomizer;
         random_string.erase(tmp_randomizer % random_string.size(), 1);
         random_string = random_string + alpha[tmp_randomizer % alpha.size()];
         return random_string;
     }
-    
+
     std::atomic_int64_t randomizer;
     std::string alpha;
     std::string random_string;
@@ -34,7 +34,7 @@ struct Params{
         input = "";
         log_level = "";
         threads = default_threads;
-        
+
         out = "dbhr-storage.db";
     }
     Params(std::string _input, std::string _log_level,
@@ -94,12 +94,12 @@ public:
         notes_in_work.store(0);
         threads = parameters.threads;
         out = parameters.out;
-        
+
         cf_names_are_ready.store(false);
-        
+
         download_finished = false;
         hashing_finished = false;
-        
+
         processing_queue = new std::queue <hash_this>;
         output_queue = new std::queue <print_this>;
     }
@@ -107,7 +107,7 @@ public:
         delete processing_queue;
         delete output_queue;
     }
-    
+
 private:
     void feel_db(std::string name){
         Options options;
@@ -116,17 +116,17 @@ private:
         Status s;
         std::vector <std::string> cf_names_;
         DB::ListColumnFamilies(DBOptions(), name, &cf_names_);
-        
+
         std::vector<ColumnFamilyDescriptor> column_families;
-        
+
         for (auto name : cf_names_){
             column_families.push_back(ColumnFamilyDescriptor(
-                                                             name, ColumnFamilyOptions()));
+                                        name, ColumnFamilyOptions()));
         }
         std::vector<ColumnFamilyHandle*> handles;
         s = DB::Open(DBOptions(), name, column_families, &handles, &db);
         assert(s.ok());
-        
+
         RandomString random_str;
         WriteBatch batch;
         for (uint32_t i = 0; i < column_families.size(); ++i){
@@ -148,7 +148,7 @@ private:
             assert(s.ok());
         }
         delete db;
-        
+//
     }
     void make_db(std::string name, std::vector <std::string> &cf_names_){
         Options options;
@@ -156,10 +156,10 @@ private:
         DB* db;
         Status s = DB::Open(options, name, &db);
         assert(s.ok());
-        
+
         std::vector <ColumnFamilyHandle*> handles;
         ColumnFamilyHandle* cf;
-        
+
         for (auto column_family : cf_names_) {
             if (column_family == "default")
             continue;
@@ -168,7 +168,7 @@ private:
             assert(s.ok());
             handles.push_back(cf);
         }
-        
+
         for (auto handle : handles) {
             s = db->DestroyColumnFamilyHandle(handle);
             assert(s.ok());
@@ -197,24 +197,24 @@ private:
         Status s;
         DB::ListColumnFamilies(DBOptions(), source, &cf_names);
         cf_names_are_ready.store(true);
-        
+
         std::vector<ColumnFamilyDescriptor> column_families;
-        
+
         for (auto name : cf_names){
             column_families.push_back(ColumnFamilyDescriptor(
-                                                             name, ColumnFamilyOptions()));
+                                        name, ColumnFamilyOptions()));
         }
         std::vector<ColumnFamilyHandle*> handles;
         s = DB::Open(DBOptions(), source, column_families, &handles, &db);
         assert(s.ok());
-        
+
         std::vector< Iterator* > iterators;
         s = db->NewIterators(ReadOptions(), handles, &iterators);
         assert(s.ok());
-        
+
         auto iterator_column_names = cf_names.begin();
         uint32_t i = 0;
-        for(auto it : iterators) {
+        for (auto it : iterators) {
             for (it->SeekToFirst(); it->Valid(); it->Next()) {
                 please_hash_it.push_back(hash_this(*iterator_column_names,
                                                    it->key().data(),
@@ -224,17 +224,17 @@ private:
             iterator_column_names++;
             delete it;
         }
-        
+
         for (auto handle : handles) {
             s = db->DestroyColumnFamilyHandle(handle);
             assert(s.ok());
         }
         delete db;
-        
+
         for (auto element : please_hash_it){
             while (!safe_processing.try_lock()){
                 std::this_thread::sleep_for(std::chrono::milliseconds(
-                                                                      kirill_sleeps_seconds));
+                                              kirill_sleeps_seconds));
             }
             processing_queue->push(element);
             safe_processing.unlock();
@@ -248,91 +248,91 @@ private:
         std::string str_after_hash;
         hash_this struct_before_hash;
         bool empty_queue = true;
-        
+
         while (!download_finished) {
             std::this_thread::yield();
         }
-        
+
         while (!safe_processing.try_lock()) {
             std::this_thread::sleep_for(std::chrono::milliseconds(
-                                                                  masha_sleeps_seconds));
+                                           masha_sleeps_seconds));
         }
         struct_before_hash = processing_queue->front();
         processing_queue->pop();
         empty_queue = processing_queue->empty();
         safe_processing.unlock();
-        
+
         if (!empty_queue) {
             parsing_threads->push(std::bind(&BD_Hasher::parsing_notes,
                                             this, parsing_threads));
         }
-        
+
         str_before_hash = struct_before_hash.key + struct_before_hash.value;
         picosha2::hash256_hex_string(str_before_hash, str_after_hash);
         print_this struct_after_hash(struct_before_hash.cf_name,
                                      struct_before_hash.key, str_after_hash);
-        
+
         while (!safe_output.try_lock()) {
             std::this_thread::sleep_for(std::chrono::milliseconds(
-                                                                  masha_sleeps_seconds));
+                                           masha_sleeps_seconds));
         }
         output_queue->push(struct_after_hash);
         safe_output.unlock();
-        
+
         if (download_finished.load() && empty_queue){
             hashing_finished.store(true);
             ss << "Hashing is completed!";
             log_it();
         }
     }
-    
+
     void writing_output() {
         if (!cf_names_are_ready) {
             std::this_thread::sleep_for(std::chrono::milliseconds(
-                                                                  dima_sleeps_seconds));
+                                            dima_sleeps_seconds));
         }
         make_db(out, cf_names);
         Options options;
         options.create_if_missing = true;
         DB* db;
         Status s;
-        
+
         std::vector<ColumnFamilyDescriptor> column_families;
         for (auto name : cf_names){
             column_families.push_back(ColumnFamilyDescriptor(
-                                                             name, ColumnFamilyOptions()));
+                                       name, ColumnFamilyOptions()));
         }
-        
+
         std::vector<ColumnFamilyHandle*> handles;
         s = DB::Open(DBOptions(), out, column_families, &handles, &db);
         assert(s.ok());
-        
+
         print_this object_to_print;
-        
+
         bool empty_queue = true;
         while (empty_queue) {
             while (!safe_output.try_lock()) {
                 std::this_thread::sleep_for(
-                                            std::chrono::milliseconds(dima_sleeps_seconds));
+                    std::chrono::milliseconds(dima_sleeps_seconds));
             }
             empty_queue = output_queue->empty();
             safe_output.unlock();
         }
-        
+
         while (!(hashing_finished && empty_queue)) {
             while (!safe_output.try_lock()) {
                 std::this_thread::sleep_for(std::chrono::milliseconds(
-                                                                      dima_sleeps_seconds));
+                                                dima_sleeps_seconds));
             }
             object_to_print = output_queue->front();
             output_queue->pop();
             safe_output.unlock();
-            
+
             int i = 0;
             for (auto column_family_name : cf_names) {
                 if (column_family_name == object_to_print.cf_name) {
                     s = db->Put(WriteOptions(), handles[i],
-                                Slice(object_to_print.key), Slice(object_to_print.hash));
+                     Slice(object_to_print.key), Slice(object_to_print.hash));
                     break;
                 }
                 ++i;
@@ -342,13 +342,13 @@ private:
                 empty_queue = output_queue->empty();
                 std::cout << "Notes in process: " <<
                 output_queue->size() << std::endl;
-                if(empty_queue){
+                if (empty_queue){
                     if (hashing_finished.load())
                     break;
                 }
             }
         }
-        
+
         for (auto handle : handles) {
             s = db->DestroyColumnFamilyHandle(handle);
             assert(s.ok());
@@ -357,7 +357,7 @@ private:
         ss << "End database done!!!!!!!!!!!!!!!!!!!!!!";
         log_it();
     }
-    
+
 public:
     void i_like_to_hash_it_hash_it(){
         try {
@@ -371,46 +371,46 @@ public:
             feel_db(source);
             ss << "Database successfully created";
             log_it();
-            
-            
+
+
             ctpl::thread_pool working_threads(threads);
-            
+
             working_threads.push(std::bind(&BD_Hasher::downloading_notes,
                                            this));
-            
+
             working_threads.push(std::bind(&BD_Hasher::parsing_notes,
                                            this, &working_threads));
             writing_output();
-            
+
             print_rezult(source, out);
-            
+//
         } catch (std::logic_error const& e){
             std::cout << e.what() << " was an error!";
         } catch (...){
             std::cout << "Unknown error! Ask those stupid coders:0";
         }
-        
+//
     }
-    
+
 private:
     std::string source;
     std::string log_level;
     uint32_t threads;
     std::string out;
-    
+
     std::atomic_bool cf_names_are_ready;
     std::vector <std::string> cf_names;
-    
+
     std::atomic_bool download_finished;
     std::atomic_bool hashing_finished;
     std::atomic_uint notes_in_work;
-    
+
     std::queue <hash_this> * processing_queue;
     std::mutex safe_processing;
-    
+
     std::queue <print_this> * output_queue;
     std::mutex safe_output;
-    
+
     std::stringstream ss;
 };
 
@@ -433,7 +433,7 @@ Params command_line_processor(int argc, char* argv[]){
     ("help,h", "Show help")
     ("type,t", po::value<std::string>(&task_type),
      "Select task: hash");
-    
+
     po::options_description parse_desc("Work options");
     parse_desc.add_options()
     ("log-level,l", po::value<std::string>()->default_value("error"),
@@ -449,13 +449,13 @@ Params command_line_processor(int argc, char* argv[]){
     po::variables_map vm;
     try {
         auto parsed = po::command_line_parser(argc,
-                                              argv).options(desc).allow_unregistered().run();
+               argv).options(desc).allow_unregistered().run();
         po::store(parsed, vm);
         po::notify(vm);
         if (task_type == "hash") {
             desc.add(parse_desc);
             parsed = po::command_line_parser(argc,
-                                             argv).options(desc).positional(file_name).allow_unregistered().run();
+            argv).options(desc).positional(file_name).allow_unregistered().run();
             po::store(parsed, vm);
             po::notify(vm);
             Params cmd_params = parse_cmd(vm);
